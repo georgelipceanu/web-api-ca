0c6ea6c966b4373781d3ae08996cf1845ad385c4
George <george.lipceanu@gmail.com>
2025-01-04 21:42:49 +0000

    removed unused enpoints from favourites

--- movies-api/api/favourites/index.js
+++ movies-api/api/favourites/index.js
@@ -18,43 +18,6 @@ router.get('/actors', async (req, res) => {
     res.status(200).json(actors);
 });
 
-// register(Create)/Authenticate User
-router.post('/', asyncHandler(async (req, res) => {
-    try {
-        if (!req.body.username || !req.body.password) {
-            return res.status(400).json({ success: false, msg: 'Username and password are required.' });
-        }
-        if (req.query.action === 'register') {
-            await registerUser(req, res);
-        } else {
-            await authenticateUser(req, res);
-        }
-    } catch (error) {
-        // Log the error and return a generic error message
-        console.error(error);
-        res.status(500).json({ success: false, msg: 'Internal server error.' });
-    }
-}));
-
-// Update a user
-router.put('/:id', async (req, res) => {
-    if (req.body._id) delete req.body._id;
-    const result = await User.updateOne({
-        _id: req.params.id,
-    }, req.body);
-    if (result.matchedCount) {
-        res.status(200).json({ code:200, msg: 'User Updated Sucessfully' });
-    } else {
-        res.status(404).json({ code: 404, msg: 'Unable to Update User' });
-    }
-});
-
-async function registerUser(req, res) {
-    // Add input validation logic here
-    await User.create(req.body);
-    res.status(201).json({ success: true, msg: 'User successfully created.' });
-}
-
 router.use('/movies', moviesRouter);
 router.use('/actors', actorsRouter);
 
